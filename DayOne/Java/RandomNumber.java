7a5037458f9b892240453bfd33ebb00bdff0619b
Olasunkanmi24 <ayodejisodunke23@gmail.com>
2026-05-18 17:09:38 +0100

    update

---
+++ DayOne/Java/RandomNumber.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import random;
+
+public class RandomNumber{
+
+    public static void main(String[] args){
+    
+        Scanner input = new Scanner(System.in);
+    
+        Random read = new Random();
+        
+        int correctNumber = read.nextInt();
+        
+        for(count = 1; count <= 5; count++){
+        
+            System.out.print("Enter a number between  1 and 100 you have 5 attempt: ");
+            
+        }
+        int number = input.nextInt();
+        
+        if(0 < number <= 100){
+        
+            if(number == correctNumber){
+            
+            break;
+            }
+            
+            else{
+            
+                for(int counter = 1; conuter <= 5; counter++){
+                
+                System.out.print("Enter your number: ");
+                
+                int number = input.nextInt();
+                
+                
+                
+                
+                
+                }
+            
+            }
+        }
+        
+        
+          
+        
+        
+        
+        
+        
+        
+        }
+            
+            
+            
+            
+            }
+    
+
