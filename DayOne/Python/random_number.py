65999858f8082a8d84249b6bbedafa5b47e7cf1e
Olasunkanmi24 <ayodejisodunke23@gmail.com>
2026-05-18 17:10:42 +0100

    update

---
+++ DayOne/Python/random_number.py
@@ -0,0 +1,11 @@
+import random
+number = int(input("Guess a number: "))
+number = random.randrange(1, 101)
+if(number <= 100) and (number >= 1):
+    more = 0;
+    for (count != number):
+        for number in range(1, 5):
+            if (number > ):
+            if ( number > num
+    
+    }
