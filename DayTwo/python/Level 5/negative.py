38ae5b7a7f1694ec8d29a45e6bcdabd77d9ba76f
Olasunkanmi24 <ayodejisodunke23@gmail.com>
2026-05-19 17:31:42 +0100

    update

---
+++ DayTwo/python/Level 5/negative.py
@@ -0,0 +1,12 @@
+def negative(array):
+    for count in range(len(array)):
+        if(array[count] < 0):
+            array[count] = 0
+    return array
+def zero_to_end(numbers):
+    for count in range(len(array)):
+        if(arrays[count] == 0):
+        
+        
+array = [1,-2,-3,5]
+print(negative(array))     
