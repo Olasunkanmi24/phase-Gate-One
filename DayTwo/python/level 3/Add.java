d286d3a25e831924453c7640041fe666d94d901e
Olasunkanmi24 <ayodejisodunke23@gmail.com>
2026-05-19 17:31:03 +0100

    update

---
+++ DayTwo/python/level 3/Add.java
@@ -0,0 +1,15 @@
+public class Add{
+
+    public static int AddTwoNumbers(){
+    
+    int add = int numberOne + int numberTwo;
+    
+    return add;   
+ 
+    }
+    
+    public static void main(String[] args){
+    
+    System.out.print(add);
+    }
+}
