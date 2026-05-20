4387c2487a04bd7790f394c26f1c690e3f789281
Olasunkanmi24 <ayodejisodunke23@gmail.com>
2026-05-20 16:03:16 +0100

    update

---
+++ DayThree/afternoonClass/Java/PackingSlot.java
@@ -0,0 +1,40 @@
+
+
+
+public class PackingSlot{
+
+    public static int packingSlots(int[] number){
+    
+    int slot = 0;
+        for(int count = 0; count < array.length; count++){
+        
+            if(array[count] == 0){
+            
+                slot++;
+            }
+        }
+            return slot;
+       
+    }
+    
+     public static String showCheckForFreeSpace(int[] number){
+     
+     int slot = determineFreeSlot(array);
+     
+     String check = "20 slot taken. no slot left";
+     
+     for(int counter = 1; counter < 20; counter++){
+     
+     if(slot != 0){
+     
+     int remian = array.length - slot; 
+     }
+     }
+        
+     }
+    
+       
+            
+    
+    }
+
